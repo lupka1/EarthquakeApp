6d218fab8ce9841d95145324f56a3cfb6876f6f4
Piotrek <StiepanR923@gmail.com>
2020-04-19 23:26:10 +0200

    adding button functions

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ document.getElementById('jeden').addEventListener('click', jedenNumer);
 document.getElementById('dwa').addEventListener('click', dwaNumer);
 document.getElementById('trzy').addEventListener('click', trzyNumer);
 
+function jedenNumer(e) {
+
+}
+
+function dwaNumer(e) {
+
+}
+
+function trzyNumer(e) {
+
+}
+
 function odswiezanie() {
     location.reload();
 }
